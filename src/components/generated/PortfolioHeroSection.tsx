c010535e82814da13bb2f549cbf48ba6262c47ba
rakshaa-t <rakshatated98@gmail.com>
2025-10-25 22:46:38 +0530

    Restructure desktop nav: logo at left (40px padding), nav buttons centered, social icons at right (40px padding)

--- src/components/generated/PortfolioHeroSection.tsx
+++ src/components/generated/PortfolioHeroSection.tsx
@@ -573,13 +573,14 @@ export const PortfolioHeroSection: React.FC<RakshaPortfolioProps> = (props: Raks
         </nav>
       ) : (
         /* Desktop Navigation */
-        <nav className="fixed left-1/2 -translate-x-1/2 top-[20px] z-50 w-full h-[68px]">
+        <nav className="fixed left-0 right-0 top-[20px] z-50">
           <div 
-            className="flex items-center justify-center gap-[563px] h-full"
+            className="flex items-center justify-between h-full mx-auto"
             style={{
               width: '100%',
-              paddingLeft: '12px',
-              paddingRight: '12px',
+              maxWidth: '100%',
+              paddingLeft: '40px',
+              paddingRight: '40px',
               paddingTop: '4px',
               paddingBottom: '4px',
               background: 'transparent',
@@ -601,9 +602,9 @@ export const PortfolioHeroSection: React.FC<RakshaPortfolioProps> = (props: Raks
               raks
       </div>
 
-          {/* Navigation Icons */}
+          {/* Navigation Icons - Centered */}
           <div 
-            className="flex items-start justify-center gap-[28px]"
+            className="absolute left-1/2 -translate-x-1/2 flex items-start justify-center gap-[28px]"
             style={{
               width: '236px',
               height: '60px'
@@ -839,6 +840,41 @@ export const PortfolioHeroSection: React.FC<RakshaPortfolioProps> = (props: Raks
                 height: '24px',
                 position: 'relative',
                 overflow: 'hidden'
+              }}
+            >
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M21.5859 21.375L14.0885 10.4471L14.1013 10.4574L20.8613 2.625H18.6023L13.0954 9L8.72227 2.625H2.79766L9.79723 12.8276L9.79638 12.8267L2.41406 21.375H4.67309L10.7955 14.2824L15.6613 21.375H21.5859ZM7.82719 4.32954L18.3466 19.6705H16.5564L6.02852 4.32954H7.82719Z" fill="var(--color-dark-900, #020617)" />
+              </svg>
+            </a>
+          </div>
+
+          {/* Social Icons - Right Side */}
+          <div className="flex items-center gap-[16px]">
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+              className="opacity-40 hover:opacity-60 transition-opacity"
+              style={{
+                width: '29px',
+                height: '29px'
+              }}
+            >
+              <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22.9583 3.625C23.5993 3.625 24.214 3.87961 24.6672 4.33283C25.1204 4.78604 25.375 5.40073 25.375 6.04167V22.9583C25.375 23.5993 25.1204 24.214 24.6672 24.6672C24.214 25.1204 23.5993 25.375 22.9583 25.375H6.04167C5.40073 25.375 4.78604 25.1204 4.33283 24.6672C3.87961 24.214 3.625 23.5993 3.625 22.9583V6.04167C3.625 5.40073 3.87961 4.78604 4.33283 4.33283C4.78604 3.87961 5.40073 3.625 6.04167 3.625H22.9583ZM22.3542 22.3542V15.95C22.3542 14.9053 21.9391 13.9033 21.2004 13.1646C20.4617 12.4259 19.4597 12.0108 18.415 12.0108C17.3879 12.0108 16.1917 12.6392 15.6117 13.5817V12.2404H12.2404V22.3542H15.6117V16.3971C15.6117 15.4667 16.3608 14.7054 17.2913 14.7054C17.7399 14.7054 18.1702 14.8836 18.4874 15.2009C18.8047 15.5181 18.9829 15.9484 18.9829 16.3971V22.3542H22.3542ZM8.31333 10.3433C8.85172 10.3433 9.36806 10.1295 9.74876 9.74876C10.1295 9.36806 10.3433 8.85172 10.3433 8.31333C10.3433 7.18958 9.43708 6.27125 8.31333 6.27125C7.77174 6.27125 7.25233 6.4864 6.86936 6.86936C6.4864 7.25233 6.27125 7.77174 6.27125 8.31333C6.27125 9.43708 7.18958 10.3433 8.31333 10.3433ZM9.99292 22.3542V12.2404H6.64583V22.3542H9.99292Z" fill="var(--color-dark-900, #020617)" />
+              </svg>
+            </a>
+
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="X (Twitter)"
+              className="opacity-40 hover:opacity-60 transition-opacity"
+              style={{
+                width: '24px',
+                height: '24px'
               }}
             >
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
